9f4e1914b457bc69aa06aeebecbb51a8db7f34d3
Cyro Dubeux <xorycx@gmail.com>
2021-03-29 16:22:55 -0300

    added csv pkg tests

---
+++ pkg/csv/csv_test.go
@@ -0,0 +1,23 @@
+package csv_test
+
+import (
+	"testing"
+
+	"github.com/cyruzin/clean_architecture/pkg/csv"
+	"github.com/cyruzin/clean_architecture/pkg/util"
+)
+
+var filePath = util.PathBuilder("./assets/routes.csv")
+
+func TestRead(t *testing.T) {
+	_, err := csv.Read(filePath)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestWrite(t *testing.T) {
+	if err := csv.Write(filePath, "RJ,SP,5"); err != nil {
+		t.Error(err)
+	}
+}
